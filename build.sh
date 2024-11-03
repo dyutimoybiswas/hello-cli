152d33028fbf43df430584fc5e8d9fee2fb7c292
Dyutimoy Biswas <mail.dybiswas@gmail.com>
2024-11-03 11:43:43 +0530

    added debugging info

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
-rm -rf build
-mkdir build
-cd build
-cmake ..
-make
+if [ $1 = debug ]; then
+    cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug
+    cmake --build build
+else
+    rm -rf build
+    mkdir build
+    cd build
+    cmake ..
+    make
+fi
