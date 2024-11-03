705ed3ddfb32525b909d69bc08986f1c149f5bda
Dyutimoy Biswas <mail.dybiswas@gmail.com>
2024-11-03 11:50:22 +0530

    added debugging info

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
-if [ $1 = debug ]; then
-    cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug
-    cmake --build build
-else
+if [ $# -eq 0 ]; then
     rm -rf build
     mkdir build
     cd build
     cmake ..
     make
+elif [ $1 = debug ]; then
+    cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug
+    cmake --build build
 fi
