97fbea1be98c37d3c8987e8a1391155bc59eca58
Dyutimoy Biswas <mail.dybiswas@gmail.com>
2024-11-14 19:00:53 +0530

    updated build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 if [ $# -eq 0 ]; then
     rm -rf build
     mkdir build
-    cd build
-    cmake ..
-    make
+    cmake -S . -B build
+    cmake --build build
 elif [ $1 = debug ]; then
     cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug
     cmake --build build
